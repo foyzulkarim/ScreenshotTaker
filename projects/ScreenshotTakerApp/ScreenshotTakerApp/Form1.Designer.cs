f364d8b2cd342cf03068818a6d693cee827fb87a
Foyzul Karim <foyzulkarim@gmail.com>
2017-12-07 21:03:52 +0600

    screen capturing after time interval

--- projects/ScreenshotTakerApp/ScreenshotTakerApp/Form1.Designer.cs
+++ projects/ScreenshotTakerApp/ScreenshotTakerApp/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(502, 403);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code Coopers Time Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- projects/ScreenshotTakerApp/ScreenshotTakerApp/Form1.cs
+++ projects/ScreenshotTakerApp/ScreenshotTakerApp/Form1.cs
@@ -10,11 +10,70 @@ using System.Windows.Forms;
 
 namespace ScreenshotTakerApp
 {
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Threading;
+    using System.IO.Compression;
+
+
     public partial class Form1 : Form
     {
+        static string folder = "Screenshots";
+
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        static int alarmCounter = 1;
+        static bool exitFlag = false;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static void TimerEventProcessor(Object myObject,
+                                                EventArgs myEventArgs)
+        {
+            AddImage();            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckFolder();
+            myTimer.Tick += TimerEventProcessor;
+            myTimer.Interval = 5000;
+            myTimer.Start();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string fileName = AddImage();            
+        }
+
+        private static string AddImage()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+
+                long ticks = DateTime.Now.Ticks;
+                string filename = $@"{folder}\{ticks}.jpeg";
+                bitmap.Save(filename, ImageFormat.Jpeg);
+                return filename;
+            }
+        }
+
+        private void CheckFolder()
+        {
+            bool exists = Directory.Exists(folder);
+            if (!exists)
+            {
+                var directoryInfo = Directory.CreateDirectory(folder);
+            }
         }
     }
 }
